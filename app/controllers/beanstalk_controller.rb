409f99b4034d69fc5aa9a62cda65d542259a717e
Rebecca Poulson <rpoulson@stanford.edu>
2012-05-14 15:12:14 -0700

    Changed controllers to better controll what is displayed

--- app/controllers/beanstalk_controller.rb
+++ app/controllers/beanstalk_controller.rb
@@ -3,6 +3,7 @@ class BeanstalkController < ApplicationController
       @logos = BeanstalkCompleteLogo.find(:all)
   end
     def create
+        @logos = BeanstalkCompleteLogo.find(:all)
         if params[:view]
             @logo = Logo.new(params[:logo])
             @logo.save
@@ -12,9 +13,9 @@ class BeanstalkController < ApplicationController
             @render_logo = Logo.last
             @save_logo = BeanstalkCompleteLogo.new(params[:complete_logo])
             @save_logo.save
+            @logos = BeanstalkCompleteLogo.find(:all)
             render 'create'
         end
- 
     end
     
     def display
